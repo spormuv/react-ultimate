54e4d073268a8616dd539c0892b4d52673404ac1
Mike <spormuv@gmail.com>
2024-02-13 00:58:52 +0300

    feat: add lifting state

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -1,17 +1,23 @@
 import { useState } from 'react';
 
-const initialItems = [
-  { id: 1, description: 'Passports', quantity: 2, packed: false },
-  { id: 2, description: 'Socks', quantity: 12, packed: true },
-  { id: 3, description: 'Charger', quantity: 1, packed: false },
-];
+// const initialItems = [
+//   { id: 1, description: 'Passports', quantity: 2, packed: false },
+//   { id: 2, description: 'Socks', quantity: 12, packed: true },
+//   { id: 3, description: 'Charger', quantity: 1, packed: false },
+// ];
 
 export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems(items => [...items, item]);
+  }
+
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleAddItems} />
+      <PackingList items={items} />
       <Stats />
     </div>
   );
@@ -21,7 +27,7 @@ function Logo() {
   return <h1>🌴 Far Away 💼</h1>;
 }
 
-function Form() {
+function Form({ onAddItems }) {
   const [descr, setDescr] = useState('');
   const [quantity, setQuantity] = useState(1);
 
@@ -36,6 +42,8 @@ function Form() {
       quantity,
       packed: false,
     };
+
+    onAddItems(newItem);
 
     setDescr('');
     setQuantity(1);
@@ -67,11 +75,11 @@ function Form() {
   );
 }
 
-function PackingList() {
+function PackingList({ items }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map(item => (
+        {items.map(item => (
           <Item key={item.id} {...item}></Item>
         ))}
       </ul>
