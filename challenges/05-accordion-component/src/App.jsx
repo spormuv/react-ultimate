383063e0f5b8f9732a9a28a082d29296460e5a39
Mike <spormuv@gmail.com>
2024-02-26 00:40:07 +0300

    feat: add accordion component challenge

---
+++ challenges/05-accordion-component/src/App.jsx
@@ -0,0 +1,56 @@
+import { useState } from 'react';
+
+/* eslint-disable react/prop-types */
+const faqs = [
+  {
+    title: 'Where are these chairs assembled?',
+    text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.',
+  },
+  {
+    title: 'How long do I have to return my chair?',
+    text: 'Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.',
+  },
+  {
+    title: 'Do you ship to countries outside the EU?',
+    text: 'Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!',
+  },
+];
+
+export default function App() {
+  return (
+    <div>
+      <Accordion data={faqs} />
+    </div>
+  );
+}
+
+function Accordion({ data }) {
+  return (
+    <div className="accordion">
+      {data.map((item, ind) => (
+        <AccordionItem
+          key={ind}
+          num={ind + 1}
+          title={item.title}
+          text={item.text}
+        />
+      ))}
+    </div>
+  );
+}
+
+function AccordionItem({ num, title, text }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div
+      className={`item ${isOpen ? 'open' : ''}`}
+      onClick={() => setIsOpen(!isOpen)}
+    >
+      <p className="number">{num < 9 ? `0${num}` : num}</p>
+      <p className="title">{title}</p>
+      <p className="icon">{isOpen ? '-' : '+'}</p>
+      {isOpen && <div className="content-box">{text}</div>}
+    </div>
+  );
+}
