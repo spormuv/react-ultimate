04cb4ffa2f2d4dd8de0387db0eba318f3039a378
Mike <spormuv@gmail.com>
2024-03-14 01:35:03 +0300

    feat: improve reusability with props

--- 07-usepopcorn/src/index.js
+++ 07-usepopcorn/src/index.js
@@ -8,6 +8,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      messages={['Terrible', 'Bad', 'Ok', 'Good', 'Wonderful']}
+      defaultRating={3}
+      onSetRating
+    />
   </React.StrictMode>
 );
