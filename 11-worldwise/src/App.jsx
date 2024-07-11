45c6cf44af1e5c8a32ccd2aeb751b430e7fecbc7
Mike <spormuv@gmail.com>
2024-07-12 00:20:12 +0300

    feat: Linking Between Routes With Link  and NavLink

--- 11-worldwise/src/App.jsx
+++ 11-worldwise/src/App.jsx
@@ -1,5 +1,20 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Homepage from './pages/Homepage';
+import PageNotFound from './pages/PageNotFound';
+import Pricing from './pages/Pricing';
+import Product from './pages/Product';
+
 function App() {
-  return <div>WorldWise</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
