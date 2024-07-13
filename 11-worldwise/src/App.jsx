410109b940abdd4b0afd2c7a790b3bb9f9d88247
Mike <spormuv@gmail.com>
2024-07-13 22:53:32 +0300

    feat: Implement the Cities List

--- 11-worldwise/src/App.jsx
+++ 11-worldwise/src/App.jsx
@@ -1,16 +1,54 @@
+import { useEffect, useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import CityList from './components/CityList';
+import AppLayout from './pages/AppLayout';
 import Homepage from './pages/Homepage';
+import Login from './pages/Login';
 import PageNotFound from './pages/PageNotFound';
 import Pricing from './pages/Pricing';
 import Product from './pages/Product';
 
+const BASE_URL = 'http://localhost:8000';
+
 function App() {
+  const [cities, setCities] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchCities() {
+      try {
+        setLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch (err) {
+        alert(err.message);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route index element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
+        <Route path="login" element={<Login />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route
+            index
+            element={<CityList loading={loading} cities={cities} />}
+          />
+          <Route
+            path="cities"
+            element={<CityList loading={loading} cities={cities} />}
+          />
+          <Route path="countries" element={<p>Countries</p>} />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
