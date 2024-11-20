17e0c36b9175f0fc5653bec0a83f80db30e759ec
Mike <spormuv@gmail.com>
2024-11-21 00:49:55 +0300

    feat: Optimization Trick With children

--- 12-atomic-blog/src/App.js
+++ 12-atomic-blog/src/App.js
@@ -1,6 +1,7 @@
 import { faker } from '@faker-js/faker';
 import { createContext, useContext, useEffect, useState } from 'react';
 import { PostProvider, usePosts } from './PostContext';
+import Test from './Test';
 
 function createRandomPost() {
   return {
@@ -125,14 +126,18 @@ function List() {
   const { posts } = usePosts();
 
   return (
-    <ul>
-      {posts.map((post, i) => (
-        <li key={i}>
-          <h3>{post.title}</h3>
-          <p>{post.body}</p>
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul>
+        {posts.map((post, i) => (
+          <li key={i}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
+
+      {/* <Test /> */}
+    </>
   );
 }
 
