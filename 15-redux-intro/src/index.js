3713ab2d783e4c50507861edae2d95643c7f76b5
Mike <spormuv@gmail.com>
2024-11-29 01:52:51 +0300

    feat: add Action Creators

--- 15-redux-intro/src/index.js
+++ 15-redux-intro/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+import './store';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
--- 15-redux-intro/src/store.js
+++ 15-redux-intro/src/store.js
@@ -1,3 +1,5 @@
+import { legacy_createStore as createStore } from 'redux';
+
 const initialState = {
   balance: 0,
   loan: 0,
@@ -12,8 +14,12 @@ function reducer(state = initialState, action) {
       return { ...state, balance: state.balance - action.payload };
     case 'account/requestLoan':
       if (state.loan > 0) return state;
-      // LATER
-      return { ...state, loan: action.payload };
+      return {
+        ...state,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.purpose,
+        balance: state.balance + action.payload.amount,
+      };
     case 'account/payLoan':
       return {
         ...state,
@@ -25,3 +31,41 @@ function reducer(state = initialState, action) {
       return state;
   }
 }
+
+// Store
+const store = createStore(reducer);
+
+// store.dispatch({ type: 'account/deposit', payload: 500 });
+// store.dispatch({ type: 'account/withdraw', payload: 200 });
+// store.dispatch({
+//   type: 'account/requestLoan',
+//   payload: { amount: 1000, purpose: 'Buy a car' },
+// });
+// store.dispatch({ type: 'account/payLoan' });
+
+// Action creators
+function deposit(amount) {
+  return { type: 'account/deposit', payload: amount };
+}
+
+function withdraw(amount) {
+  return { type: 'account/withdraw', payload: amount };
+}
+
+function requestLoan(amount, purpose) {
+  return {
+    type: 'account/requestLoan',
+    payload: { amount, purpose },
+  };
+}
+
+function payLoan() {
+  return { type: 'account/payLoan' };
+}
+
+store.dispatch(deposit(500));
+store.dispatch(withdraw(200));
+store.dispatch(requestLoan(1000, 'Buy a car'));
+store.dispatch(payLoan());
+
+console.log(store.getState());
