88fa9862b76d6891f36d3f2a81c6ad11a1f10453
Mike <spormuv@gmail.com>
2024-12-03 01:35:14 +0300

    feat: Create the Customer Slice with RTK

--- 15-redux-intro/src/features/customers/customerSlice-v1.js
+++ 15-redux-intro/src/features/customers/customerSlice-v1.js
@@ -1,41 +1,9 @@
-import { createSlice } from '@reduxjs/toolkit';
-
-const initialState = {
+const initialStateCustomer = {
   fullName: '',
   nationalId: '',
   createdAt: '',
 };
 
-const customerSlice = createSlice({
-  name: 'customer',
-  initialState,
-  reducers: {
-    createCustomer: {
-      prepare(fullName, nationalId) {
-        return {
-          payload: {
-            fullName,
-            nationalId,
-            createdAt: new Date().toISOString(),
-          },
-        };
-      },
-      reducer(state, action) {
-        state.fullName = action.payload.fullName;
-        state.nationalId = action.payload.nationalId;
-        state.createdAt = action.payload.createdAt;
-      },
-    },
-    updateName(state, action) {
-      state.fullName = action.payload;
-    },
-  },
-});
-
-export const { createCustomer, updateName } = customerSlice.actions;
-
-export default customerSlice.reducer;
-/*
 export default function customerReducer(state = initialStateCustomer, action) {
   switch (action.type) {
     case 'customer/createCustomer':
@@ -66,4 +34,3 @@ export function updateName(fullName) {
     payload: fullName,
   };
 }
-*/
