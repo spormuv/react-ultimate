80ae6c69d15920badb35ec0abb35c1497488bf37
Mike <spormuv@gmail.com>
2024-12-06 01:59:04 +0300

    feat: Build the App Layout with React Router 6

--- 16-fast-react-pizza/src/App.jsx
+++ 16-fast-react-pizza/src/App.jsx
@@ -1,5 +1,42 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import Cart from './features/cart/Cart';
+import Menu from './features/menu/Menu';
+import CreateOrder from './features/order/CreateOrder';
+import Order from './features/order/Order';
+import AppLayout from './ui/AppLayout';
+import Home from './ui/Home';
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/menu',
+        element: <Menu />,
+      },
+      {
+        path: '/cart',
+        element: <Cart />,
+      },
+      {
+        path: '/order/new',
+        element: <CreateOrder />,
+      },
+      {
+        path: '/order/:orderId',
+        element: <Order />,
+      },
+    ],
+  },
+]);
+
 function App() {
-  return <div>Hello</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
