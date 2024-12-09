985c4701651caeda98fbb0807c57c097b4234091
Mike <spormuv@gmail.com>
2024-12-10 01:42:20 +0300

    feat: add Fetching, Loading,  Handling Errors

--- 16-fast-react-pizza/src/App.jsx
+++ 16-fast-react-pizza/src/App.jsx
@@ -1,42 +1,60 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import Cart from './features/cart/Cart';
-import Menu from './features/menu/Menu';
+import Menu, { loader as menuLoader } from './features/menu/Menu';
 import CreateOrder from './features/order/CreateOrder';
 import Order from './features/order/Order';
 import AppLayout from './ui/AppLayout';
+import Error from './ui/Error';
 import Home from './ui/Home';
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      element: <AppLayout />,
+      errorElement: <Error />,
+
+      children: [
+        {
+          path: '/',
+          element: <Home />,
+        },
+        {
+          path: '/menu',
+          element: <Menu />,
+          loader: menuLoader,
+          errorElement: <Error />,
+        },
+        {
+          path: '/cart',
+          element: <Cart />,
+        },
+        {
+          path: '/order/new',
+          element: <CreateOrder />,
+        },
+        {
+          path: '/order/:orderId',
+          element: <Order />,
+        },
+      ],
+    },
+  ],
   {
-    element: <AppLayout />,
-    children: [
-      {
-        path: '/',
-        element: <Home />,
-      },
-      {
-        path: '/menu',
-        element: <Menu />,
-      },
-      {
-        path: '/cart',
-        element: <Cart />,
-      },
-      {
-        path: '/order/new',
-        element: <CreateOrder />,
-      },
-      {
-        path: '/order/:orderId',
-        element: <Order />,
-      },
-    ],
-  },
-]);
+    future: {
+      v7_fetcherPersist: true,
+      v7_normalizeFormMethod: true,
+      v7_partialHydration: true,
+      v7_relativeSplatPath: true,
+      v7_skipActionErrorRevalidation: true,
+    },
+  }
+);
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <RouterProvider router={router} future={{ v7_startTransition: true }} />
+  );
 }
 
 export default App;
