9a88c163c27a6d4109706d164bb20e1557ba45ec
Mike <spormuv@gmail.com>
2024-12-13 01:34:01 +0300

    feat: Style Colors and Text

---
+++ 16-fast-react-pizza/src/features/user/Username.jsx
@@ -0,0 +1,5 @@
+function Username() {
+  return <div className="text-sm font-semibold">Spormuv</div>;
+}
+
+export default Username;
--- 16-fast-react-pizza/src/ui/Header.jsx
+++ 16-fast-react-pizza/src/ui/Header.jsx
@@ -1,14 +1,16 @@
-import { Link } from 'react-router-dom';
-import SearchOrder from '../features/order/SearchOrder';
+import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
+import Username from "../features/user/Username";
 
 function Header() {
   return (
-    <header>
-      <Link to="/">Fast React Pizza Co.</Link>
+    <header className="bg-yellow-500 uppercase">
+      <Link className="tracking-widest" to="/">
+        Fast React Pizza Co.
+      </Link>
 
       <SearchOrder />
-
-      <p>spormuv</p>
+      <Username />
     </header>
   );
 }
