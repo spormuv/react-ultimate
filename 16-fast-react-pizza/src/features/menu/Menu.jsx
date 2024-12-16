a8e76df9897ad47e73d0aa4e72698d1ef488147e
Mike <spormuv@gmail.com>
2024-12-17 02:22:28 +0300

    feat: styling the Menu, Cart

--- 16-fast-react-pizza/src/features/menu/Menu.jsx
+++ 16-fast-react-pizza/src/features/menu/Menu.jsx
@@ -1,13 +1,13 @@
-import { useLoaderData } from 'react-router-dom';
-import { getMenu } from '../../services/apiRestaurant';
-import MenuItem from './MenuItem';
+import { useLoaderData } from "react-router-dom";
+import { getMenu } from "../../services/apiRestaurant";
+import MenuItem from "./MenuItem";
 
 function Menu() {
   const menu = useLoaderData();
 
   return (
-    <ul>
-      {menu.map(pizza => (
+    <ul className="divide-y divide-stone-200 px-2">
+      {menu.map((pizza) => (
         <MenuItem key={pizza.id} pizza={pizza} />
       ))}
     </ul>
