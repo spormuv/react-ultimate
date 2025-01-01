55be2d3341beb2a5c017b919bd2a7e44d6dc739a
Mike <spormuv@gmail.com>
2025-01-02 00:20:01 +0300

    feat: implement Fetching & Updating Data Without Navigation, useFetcher

--- 16-fast-react-pizza/src/App.jsx
+++ 16-fast-react-pizza/src/App.jsx
@@ -6,6 +6,7 @@ import CreateOrder, {
   action as createOrderAction,
 } from './features/order/CreateOrder';
 import Order, { loader as orderLoader } from './features/order/Order';
+import { action as updateOrderAction } from './features/order/UpdateOrder';
 import AppLayout from './ui/AppLayout';
 import Error from './ui/Error';
 import Home from './ui/Home';
@@ -41,6 +42,7 @@ const router = createBrowserRouter(
           element: <Order />,
           loader: orderLoader,
           errorElement: <Error />,
+          action: updateOrderAction,
         },
       ],
     },
@@ -53,7 +55,7 @@ const router = createBrowserRouter(
       v7_relativeSplatPath: true,
       v7_skipActionErrorRevalidation: true,
     },
-  }
+  },
 );
 
 function App() {
--- 16-fast-react-pizza/src/features/order/Order.jsx
+++ 16-fast-react-pizza/src/features/order/Order.jsx
@@ -1,6 +1,7 @@
 // Test ID: IIDSAT
 
-import { useLoaderData } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useFetcher, useLoaderData } from 'react-router-dom';
 import { getOrder } from '../../services/apiRestaurant';
 import {
   calcMinutesLeft,
@@ -8,10 +9,17 @@ import {
   formatDate,
 } from '../../utils/helpers';
 import OrderItem from './OrderItem';
+import UpdateOrder from './UpdateOrder';
 
 function Order() {
   // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
   const order = useLoaderData();
+
+  const fetcher = useFetcher();
+
+  useEffect(() => {
+    if (!fetcher.data && fetcher.state === 'idle') fetcher.load('/menu');
+  }, [fetcher]);
 
   const {
     id,
@@ -55,7 +63,15 @@ function Order() {
 
       <ul className="divide-y divide-stone-200 border-b border-t">
         {cart.map((item) => (
-          <OrderItem item={item} key={item.pizzaId} />
+          <OrderItem
+            item={item}
+            key={item.pizzaId}
+            isLoadingIngredients={fetcher.state === 'loading'}
+            ingredients={
+              fetcher?.data?.find((el) => el.id === item.pizzaId)
+                ?.ingredients ?? []
+            }
+          />
         ))}
       </ul>
 
@@ -72,6 +88,8 @@ function Order() {
           To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}
         </p>
       </div>
+
+      {!priority && <UpdateOrder order={order} />}
     </div>
   );
 }
