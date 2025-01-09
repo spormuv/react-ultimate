07fe298bb16c64f7dc205c82146e120483642fcb
Mike <spormuv@gmail.com>
2025-01-10 01:05:22 +0300

    feat:  Set Up React Query

--- 17-the-wild-oasis/src/App.jsx
+++ 17-the-wild-oasis/src/App.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import Account from './pages/Account';
 import Bookings from './pages/Bookings';
 import Cabins from './pages/Cabins';
@@ -11,10 +13,21 @@ import Users from './pages/Users';
 import GlobalStyles from './styles/GlobalStyles';
 import Applayout from './ui/Applayout';
 
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			staleTime: 60 * 1000,
+		},
+	},
+});
+
 const App = () => {
 	return (
-		<>
+		<QueryClientProvider client={queryClient}>
+			<ReactQueryDevtools initialIsOpen={false} />
+
 			<GlobalStyles />
+
 			<BrowserRouter>
 				<Routes>
 					<Route element={<Applayout />}>
@@ -31,7 +44,7 @@ const App = () => {
 					<Route path='*' element={<PageNotFound />} />
 				</Routes>
 			</BrowserRouter>
-		</>
+		</QueryClientProvider>
 	);
 };
 
