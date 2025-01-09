0d019b63e1f416a7aaaa692c80dc2cad9153354f
Mike <spormuv@gmail.com>
2025-01-10 00:45:53 +0300

    feat: Connect Supabase

--- 17-the-wild-oasis/src/pages/Cabins.jsx
+++ 17-the-wild-oasis/src/pages/Cabins.jsx
@@ -1,13 +1,24 @@
-import Heading from "../ui/Heading";
-import Row from "../ui/Row";
+import { useEffect } from 'react';
+import { getCabins } from '../services/apiCabins';
+import Heading from '../ui/Heading';
+import Row from '../ui/Row';
 
 function Cabins() {
-  return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-    </Row>
-  );
+	useEffect(() => {
+		getCabins().then(data => console.log(data));
+	}, []);
+
+	return (
+		<Row type='horizontal'>
+			<Heading as='h1'>All cabins</Heading>
+			<p>TEST</p>
+			<img
+				src='https://jqxwjmcyhunyjcgjnobu.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg'
+				alt='cabin'
+				width={300}
+			/>
+		</Row>
+	);
 }
 
 export default Cabins;
---
+++ 17-the-wild-oasis/src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://jqxwjmcyhunyjcgjnobu.supabase.co';
+const supabaseKey =
+	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpxeHdqbWN5aHVueWpjZ2pub2J1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzYzNzE3MDIsImV4cCI6MjA1MTk0NzcwMn0.uYbfqIIwTR1tHEEIi3-aX_HheIY4M-s5wEnwCvVQ8xY';
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
