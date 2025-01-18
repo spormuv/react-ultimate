d27bf09774aa7965617cb60881ed6f20c4b5c3ca
Mike <spormuv@gmail.com>
2025-01-19 00:46:36 +0300

    feat: add Uploading Images to Supabase

--- 17-the-wild-oasis/src/features/cabins/CreateCabinForm.jsx
+++ 17-the-wild-oasis/src/features/cabins/CreateCabinForm.jsx
@@ -26,7 +26,7 @@ function CreateCabinForm() {
   });
 
   function onSubmit(data) {
-    mutate(data);
+    mutate({ ...data, image: data.image[0] });
   }
 
   function onError(err) {
@@ -103,7 +103,11 @@ function CreateCabinForm() {
       </FormRow>
 
       <FormRow label='Cabin photo'>
-        <FileInput id='image' accept='image/*' />
+        <FileInput
+          id='image'
+          accept='image/*'
+          {...register('image', { required: 'This field is required' })}
+        />
       </FormRow>
 
       <FormRow>
--- 17-the-wild-oasis/src/services/apiCabins.js
+++ 17-the-wild-oasis/src/services/apiCabins.js
@@ -1,4 +1,4 @@
-import supabase from './supabase';
+import supabase, { supabaseUrl } from './supabase';
 
 export async function getCabins() {
   const { data, error } = await supabase.from('cabins').select('*');
@@ -12,11 +12,35 @@ export async function getCabins() {
 }
 
 export async function createCabin(newCabin) {
-  const { data, error } = await supabase.from('cabins').insert([newCabin]);
+  const imageName = `${Math.random()}-${newCabin.image.name}`.replaceAll(
+    '/',
+    ''
+  );
+
+  const imagePath = `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
+
+  // 1. Create a new cabin
+  const { data, error } = await supabase
+    .from('cabins')
+    .insert([{ ...newCabin, image: imagePath }]);
 
   if (error) {
     console.error(error);
     throw new Error('Cabin could not be created');
+  }
+
+  // 2. Upload the image
+  const { error: storageError } = await supabase.storage
+    .from('cabin-images')
+    .upload(imageName, newCabin.image);
+
+  // 3. Delete the cabin if the image upload fails
+  if (storageError) {
+    await supabase.from('cabins').delete().eq('id', data.id);
+    console.error(storageError);
+    throw new Error(
+      'Cabin image could not be uploaded and the cabin was not created'
+    );
   }
 
   return data;
