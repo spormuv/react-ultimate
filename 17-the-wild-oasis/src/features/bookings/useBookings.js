6b2d646ed71be38ca759fb43559c0a00a14a5448
Mike <spormuv@gmail.com>
2025-02-03 00:11:33 +0300

    feat: add Filtering and Sorting Bookings

--- 17-the-wild-oasis/src/features/bookings/useBookings.js
+++ 17-the-wild-oasis/src/features/bookings/useBookings.js
@@ -1,14 +1,29 @@
 import { useQuery } from '@tanstack/react-query';
+import { useSearchParams } from 'react-router-dom';
 import { getBookings } from '../../services/apiBookings';
 
 export function useBookings() {
+  const [searchParams] = useSearchParams();
+
+  // Filter
+  const filterValue = searchParams.get('status');
+  const filter =
+    !filterValue || filterValue === 'all'
+      ? null
+      : { field: 'status', value: filterValue, method: 'eq' };
+
+  // Sort
+  const sortByRaw = searchParams.get('sortBy') || 'startDate-desc';
+  const [field, direction] = sortByRaw.split('-');
+  const sortBy = { field, direction };
+
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ['bookings'],
-    queryFn: getBookings,
+    queryKey: ['bookings', filter, sortBy],
+    queryFn: () => getBookings({ filter, sortBy }),
   });
 
   return { bookings, isLoading, error };
