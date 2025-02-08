8748aa190d072cccb0b358fde4635081fa3621f7
Mike <spormuv@gmail.com>
2025-02-08 23:53:44 +0300

    feat: implement User Logout functionality

--- 17-the-wild-oasis/src/features/authentication/useLogin.js
+++ 17-the-wild-oasis/src/features/authentication/useLogin.js
@@ -4,14 +4,14 @@ import { useNavigate } from 'react-router-dom';
 import { login as loginApi } from '../../services/apiAuth';
 
 export function useLogin() {
-  // const queryClient = useQueryClient();
+  const queryClient = useQueryClient();
   const navigate = useNavigate();
 
   const { mutate: login, isLoading } = useMutation({
     mutationFn: ({ email, password }) => loginApi({ email, password }),
     onSuccess: user => {
-      // queryClient.setQueriesData(['user'], user);
-      navigate('/dashboard');
+      queryClient.setQueryData(['user'], user.user);
+      navigate('/dashboard', { replace: true });
     },
     onError: err => {
       console.log('ERROR', err);
