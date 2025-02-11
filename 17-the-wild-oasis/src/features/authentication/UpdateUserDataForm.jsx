3416e1d25a3f155d4382ef62d1e53b1c9dd5da77
Mike <spormuv@gmail.com>
2025-02-12 00:33:46 +0300

    feat: add Updating User Data and Password

--- 17-the-wild-oasis/src/features/authentication/UpdateUserDataForm.jsx
+++ 17-the-wild-oasis/src/features/authentication/UpdateUserDataForm.jsx
@@ -6,6 +6,7 @@ import Form from '../../ui/Form';
 import FormRow from '../../ui/FormRow';
 import Input from '../../ui/Input';
 
+import { useUpdateUser } from './useUpdateUser';
 import { useUser } from './useUser';
 
 function UpdateUserDataForm() {
@@ -17,11 +18,28 @@ function UpdateUserDataForm() {
     },
   } = useUser();
 
+  const { updateUser, isUpdating } = useUpdateUser();
+
   const [fullName, setFullName] = useState(currentFullName);
   const [avatar, setAvatar] = useState(null);
 
   function handleSubmit(e) {
     e.preventDefault();
+    if (!fullName) return;
+    updateUser(
+      { fullName, avatar },
+      {
+        onSuccess: () => {
+          setAvatar(null);
+          e.target.reset();
+        },
+      }
+    );
+  }
+
+  function handleCancel() {
+    setFullName(currentFullName);
+    setAvatar(null);
   }
 
   return (
@@ -29,26 +47,36 @@ function UpdateUserDataForm() {
       <FormRow label='Email address'>
         <Input value={email} disabled />
       </FormRow>
+
       <FormRow label='Full name'>
         <Input
           type='text'
           value={fullName}
           onChange={e => setFullName(e.target.value)}
           id='fullName'
+          disabled={isUpdating}
         />
       </FormRow>
+
       <FormRow label='Avatar image'>
         <FileInput
           id='avatar'
           accept='image/*'
           onChange={e => setAvatar(e.target.files[0])}
+          disabled={isUpdating}
         />
       </FormRow>
+
       <FormRow>
-        <Button type='reset' variation='secondary'>
+        <Button
+          type='reset'
+          variation='secondary'
+          disabled={isUpdating}
+          onClick={handleCancel}
+        >
           Cancel
         </Button>
-        <Button>Update account</Button>
+        <Button disabled={isUpdating}>Update account</Button>
       </FormRow>
     </Form>
   );
