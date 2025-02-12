bedd9afe20f8090358ecf14e61843be2473dac39
Mike <spormuv@gmail.com>
2025-02-13 01:02:53 +0300

    feat: Implement Dark Mode With CSS Variables

--- 17-the-wild-oasis/src/App.jsx
+++ 17-the-wild-oasis/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Toaster } from 'react-hot-toast';
+import { DarkModeProvider } from './context/DarkModeContext';
 import Account from './pages/Account';
 import Booking from './pages/Booking';
 import Bookings from './pages/Bookings';
@@ -28,57 +29,59 @@ const queryClient = new QueryClient({
 
 const App = () => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen={false} />
+    <DarkModeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
 
-      <GlobalStyles />
+        <GlobalStyles />
 
-      <BrowserRouter>
-        <Routes>
-          <Route
-            element={
-              <ProtectedRoute>
-                <Applayout />
-              </ProtectedRoute>
-            }
-          >
-            <Route index element={<Navigate replace to='dashboard' />} />
-            <Route path='dashboard' element={<Dashboard />} />
-            <Route path='bookings' element={<Bookings />} />
-            <Route path='bookings/:bookingId' element={<Booking />} />
-            <Route path='checkin/:bookingId' element={<Checkin />} />
-            <Route path='cabins' element={<Cabins />} />
-            <Route path='users' element={<Users />} />
-            <Route path='settings' element={<Settings />} />
-            <Route path='account' element={<Account />} />
-          </Route>
+        <BrowserRouter>
+          <Routes>
+            <Route
+              element={
+                <ProtectedRoute>
+                  <Applayout />
+                </ProtectedRoute>
+              }
+            >
+              <Route index element={<Navigate replace to='dashboard' />} />
+              <Route path='dashboard' element={<Dashboard />} />
+              <Route path='bookings' element={<Bookings />} />
+              <Route path='bookings/:bookingId' element={<Booking />} />
+              <Route path='checkin/:bookingId' element={<Checkin />} />
+              <Route path='cabins' element={<Cabins />} />
+              <Route path='users' element={<Users />} />
+              <Route path='settings' element={<Settings />} />
+              <Route path='account' element={<Account />} />
+            </Route>
 
-          <Route path='login' element={<Login />} />
-          <Route path='*' element={<PageNotFound />} />
-        </Routes>
-      </BrowserRouter>
+            <Route path='login' element={<Login />} />
+            <Route path='*' element={<PageNotFound />} />
+          </Routes>
+        </BrowserRouter>
 
-      <Toaster
-        position='top-center'
-        gutter={12}
-        containerStyle={{ margin: '8px' }}
-        toastOptions={{
-          success: {
-            duration: 3000,
-          },
-          error: {
-            duration: 5000,
-          },
-          style: {
-            fontSize: '16px',
-            maxWidth: '500px',
-            padding: '16px 24px',
-            backgroundColor: 'var(--color-grey-0)',
-            color: 'var(--color-grey-700)',
-          },
-        }}
-      />
-    </QueryClientProvider>
+        <Toaster
+          position='top-center'
+          gutter={12}
+          containerStyle={{ margin: '8px' }}
+          toastOptions={{
+            success: {
+              duration: 3000,
+            },
+            error: {
+              duration: 5000,
+            },
+            style: {
+              fontSize: '16px',
+              maxWidth: '500px',
+              padding: '16px 24px',
+              backgroundColor: 'var(--color-grey-0)',
+              color: 'var(--color-grey-700)',
+            },
+          }}
+        />
+      </QueryClientProvider>
+    </DarkModeProvider>
   );
 };
 
--- 17-the-wild-oasis/src/ui/HeaderMenu.jsx
+++ 17-the-wild-oasis/src/ui/HeaderMenu.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import Logout from '../features/authentication/Logout';
 import ButtonIcon from './ButtonIcon';
+import DarkModeToggle from './DarkModeToggle';
 
 const StyledHeaderMenu = styled.ul`
   display: flex;
@@ -19,6 +20,11 @@ const HeaderMenu = () => {
           <HiOutlineUser />
         </ButtonIcon>
       </li>
+
+      <li>
+        <DarkModeToggle />
+      </li>
+
       <li>
         <Logout />
       </li>
