b6c29debea20c26ef6e7ea0ecde0d2da01754b9e
Mike <spormuv@gmail.com>
2025-02-23 01:42:37 +0300

    feat: Display a Loading Indicator

--- 19-the-wild-oasis-website/app/cabins/page.js
+++ 19-the-wild-oasis-website/app/cabins/page.js
@@ -1,3 +1,18 @@
-export default function Page() {
-  return <h1>Cabins page</h1>;
+import Counter from '../components/Counter';
+
+export default async function Page() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await res.json();
+
+  return (
+    <div>
+      <ul>
+        {data.map(user => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+
+      <Counter users={data} />
+    </div>
+  );
 }
