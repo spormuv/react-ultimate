7a092a025d31a7af6fbfa3b56d528a8b646960dd
Mike <spormuv@gmail.com>
2025-03-26 00:36:59 +0300

    feat: Sharing State Between Client and Server with The URL

--- 19-the-wild-oasis-website/app/cabins/page.js
+++ 19-the-wild-oasis-website/app/cabins/page.js
@@ -1,5 +1,6 @@
 import { Suspense } from 'react';
 import CabinList from '../_components/CabinList';
+import Filter from '../_components/Filter';
 import Spinner from '../_components/Spinner';
 
 export const revalidate = 3600;
@@ -8,7 +9,9 @@ export const metadata = {
   title: 'Cabins',
 };
 
-export default function Page() {
+export default function Page({ searchParams }) {
+  const filter = searchParams?.capacity ?? 'all';
+
   return (
     <div>
       <h1 className='text-4xl mb-5 text-accent-400 font-medium'>
@@ -23,8 +26,12 @@ export default function Page() {
         Welcome to paradise.
       </p>
 
-      <Suspense fallback={<Spinner />}>
-        <CabinList />
+      <div className='flex justify-end mb-8'>
+        <Filter />
+      </div>
+
+      <Suspense fallback={<Spinner />} key={filter}>
+        <CabinList filter={filter} />
       </Suspense>
     </div>
   );
