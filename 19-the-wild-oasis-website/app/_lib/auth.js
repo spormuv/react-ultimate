fd4d7b685449ccbf384a31b9a9c1fb0a6cfdd880
Mike <spormuv@gmail.com>
2025-04-02 01:22:03 +0300

    feat: build Custom Sign In & Out Buttons

--- 19-the-wild-oasis-website/app/_lib/auth.js
+++ 19-the-wild-oasis-website/app/_lib/auth.js
@@ -13,9 +13,14 @@ const authConfig = {
       return !!auth?.user;
     },
   },
+  pages: {
+    signIn: '/login',
+  },
 };
 
 export const {
   handlers: { GET, POST },
   auth,
+  signIn,
+  signOut,
 } = NextAuth(authConfig);
