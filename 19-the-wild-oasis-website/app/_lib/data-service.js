561422fd2265143ba431a80cf0f2291bd9e15026
Mike <spormuv@gmail.com>
2025-04-09 00:03:16 +0300

    feat: implement Creating a New Reservation

--- 19-the-wild-oasis-website/app/_lib/data-service.js
+++ 19-the-wild-oasis-website/app/_lib/data-service.js
@@ -163,6 +163,7 @@ export async function createGuest(newGuest) {
   return data;
 }
 
+/*
 export async function createBooking(newBooking) {
   const { data, error } = await supabase
     .from('bookings')
@@ -183,7 +184,7 @@ export async function createBooking(newBooking) {
 // UPDATE
 
 // The updatedFields is an object which should ONLY contain the updated data
-/*
+
 export async function updateGuest(id, updatedFields) {
   const { data, error } = await supabase
     .from('guests')
